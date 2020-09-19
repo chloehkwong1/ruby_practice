985a01d1521cbab1e35761612873cafa0d615204
Chloe <chloe.wong@madetech.com>
2020-09-19 23:48:00 +0100

    added derived class

--- palindrome.rb
+++ palindrome.rb
@@ -1,13 +1,29 @@
 class Phrase < String
 
+    def processor(string)
+        string.downcase
+    end
+
+    def processed_content
+        processor(self)
+    end
+
     # returns true for a palindrome and false otherwise
     def palindrome?
-        processed_content = downcase
         processed_content == processed_content.reverse
     end
 
-    def louder
-        upcase
-    end
 end
 
+class TranslatedPhrase < Phrase
+    attr_accessor :translation
+
+    def initialize(content, translation)
+        super(content)
+        @translation = translation
+    end
+
+    def processed_content
+        processor(translation)
+    end
+end
