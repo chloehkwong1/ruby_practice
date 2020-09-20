0ba133881fe5a5f6c306ca2379dae125c345a3b9
Chloe <chloe.wong@madetech.com>
2020-09-20 16:18:17 +0100

    using regex to add method to String

--- palindrome.rb
+++ palindrome.rb
@@ -1,29 +1,36 @@
-class Phrase < String
+# class Phrase < String
 
-    def processor(string)
-        string.downcase
-    end
+#     def processor(string)
+#         string.downcase
+#     end
 
-    def processed_content
-        processor(self)
-    end
+#     def processed_content
+#         processor(self)
+#     end
 
-    # returns true for a palindrome and false otherwise
-    def palindrome?
-        processed_content == processed_content.reverse
-    end
+#     # returns true for a palindrome and false otherwise
+#     def palindrome?
+#         processed_content == processed_content.reverse
+#     end
 
-end
+# end
 
-class TranslatedPhrase < Phrase
-    attr_accessor :translation
+# class TranslatedPhrase < Phrase
+#     attr_accessor :translation
 
-    def initialize(content, translation)
-        super(content)
-        @translation = translation
-    end
+#     def initialize(content, translation)
+#         super(content)
+#         @translation = translation
+#     end
 
-    def processed_content
-        processor(translation)
+#     def processed_content
+#         processor(translation)
+#     end
+# end
+
+class String
+    def blank?
+        #self.match?(/^\s*$/)
+        match?(/^\s*$/)
     end
 end
